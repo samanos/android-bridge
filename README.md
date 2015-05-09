a9d6101fe58034d3ab46c37e5bf2bab836fd8735
Martynas Mickevičius <mmartynas@gmail.com>
2015-05-09 22:42:16 +0300

    A working Java only solution using UsbManager

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ### Building
 
-1. Create a symlink `libusb` that points to the [libusb](https://github.com/libusb/libusb) source code. Current code is compiled and ran with libusb version `1.0.19`.
-2. Create a `local.properties` file that contains path to Android SDK and Android NDK:
+1. Create a `local.properties` file that contains path to Android SDK:
 
         sdk.dir=/path/to/android-sdk-linux
-        ndk.dir=/path/to/android-ndk-r10d
 
-3. Run `ndk-build` to compile C code.
-4. Run `gradle` to compile Java code and build an APK.
+2. Run `gradle build` to compile Java code and build an APK.
+3. To install built APK to the device run:
+
+        adb install -r build/outputs/apk/android-bridge-debug-unaligned.apk
--- src/main/java/io/github/samanos/AndroidBridge.java
+++ src/main/java/io/github/samanos/AndroidBridge.java
@@ -1,25 +1,94 @@
 package io.github.samanos;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
 import android.widget.TextView;
 import android.os.Bundle;
+import static android.hardware.usb.UsbConstants.*;
+
+import java.util.HashMap;
 
 public class AndroidBridge extends Activity
 {
+    private TextView tv = null;
+
+    private static final String TAG = "AndroidBridge";
+
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private final BroadcastReceiver permissionReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (device != null) {
+
+                            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+                            UsbInterface intf = device.getInterface(0);
+                            UsbDeviceConnection connection = manager.openDevice(device);
+                            connection.claimInterface(intf, true);
+
+                            int requestType = USB_TYPE_VENDOR | USB_DIR_IN;
+                            final byte[] buf = new byte[256];
+                            final int length = connection.controlTransfer(requestType, 1, 0, 0, buf, buf.length, 1000);
+
+                            AndroidBridge.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    tv.setText("Length: " + length + "Resp: " + bytesToHex(buf));
+                                }
+                            });
+
+                            connection.releaseInterface(intf);
+                            connection.close();
+                        }
+                    }
+                    else {
+                        Log.d(TAG, "permission denied for device " + device);
+                    }
+                }
+            }
+        }
+    };
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        TextView tv = new TextView(this);
-        tv.setText(queryForData());
+        tv = new TextView(this);
         setContentView(tv);
+
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+        UsbDevice device = deviceList.values().iterator().next();
+
+        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(permissionReceiver, filter);
+
+        manager.requestPermission(device, permissionIntent);
     }
 
-    public native String queryForData();
-
-    static {
-        System.loadLibrary("android_bridge");
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
